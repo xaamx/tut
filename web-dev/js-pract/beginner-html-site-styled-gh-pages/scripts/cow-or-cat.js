3d36f34579b314111f7bd7b3397a41a493682d7a
xaam <xaamxxx@gmail.com>
2021-10-20 17:53:47 +0500

    experimenting with javascript

---
+++ web-dev/js-pract/beginner-html-site-styled-gh-pages/scripts/cow-or-cat.js
@@ -0,0 +1,17 @@
+// declare variable to be something
+let mammal ='cow';
+// if variable is cow
+if(mammal === 'cow') {
+
+    // moo
+    alert ('moo'); }
+
+// if variable is not a cow
+else {
+    // meow
+    alert('meow');
+
+}
+
+let myVariable = document.querySelector('h1');
+alert('hi');
---
+++ web-dev/js-pract/counter-app/index.js
@@ -0,0 +1,5 @@
+// document.getElementById("count").innerText = 5
+
+let count = 5
+
+console.log(count)
